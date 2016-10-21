0a73f92063ef6ee289be9a5f212d9142112440ee
Jimmy Jenkins <mchorselessrider@gmail.com>
2016-10-21 09:25:01 +0100

    Change version

--- src/main/java/mchorse/metamorph/Metamorph.java
+++ src/main/java/mchorse/metamorph/Metamorph.java
@@ -30,7 +30,7 @@ public class Metamorph
 
     public static final String MODID = "metamorph";
     public static final String MODNAME = "Metamorph";
-    public static final String VERSION = "1.0-pre";
+    public static final String VERSION = "1.0";
 
     public static final String CLIENT_PROXY = "mchorse.metamorph.ClientProxy";
     public static final String SERVER_PROXY = "mchorse.metamorph.ServerProxy";
