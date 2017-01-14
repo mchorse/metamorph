a92d2a192c141d6d245eea7b166797fd9d7e8bb0
Jimmy Jenkins <mchorselessrider@gmail.com>
2017-01-14 09:08:09 +0000

    Better swimming

--- src/main/java/mchorse/vanilla_pack/abilities/Swim.java
+++ src/main/java/mchorse/vanilla_pack/abilities/Swim.java
@@ -2,6 +2,9 @@ package mchorse.vanilla_pack.abilities;
 
 import mchorse.metamorph.api.abilities.Ability;
 import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.util.DamageSource;
+import net.minecraft.util.math.MathHelper;
+import net.minecraft.world.EnumDifficulty;
 
 /**
  * Swim ability
@@ -15,26 +18,35 @@ public class Swim extends Ability
     @Override
     public void update(EntityLivingBase target)
     {
-        double speed = 1.25D;
-        double maxSpeed = speed * 0.75;
+        double speed = 0.35;
 
         if (target.isInWater())
         {
             if (target.moveForward != 0 || target.moveStrafing != 0)
             {
-                if (target.motionX <= maxSpeed && target.motionX >= -maxSpeed) target.motionX *= speed;
-                if (target.motionZ <= maxSpeed && target.motionZ >= -maxSpeed) target.motionZ *= speed;
-            }
+                float a = target.rotationYaw / 180 * (float) Math.PI;
 
-            if (Math.abs(target.motionY) <= maxSpeed)
-            {
-                target.motionY *= speed;
-            }
+                float f1 = MathHelper.sin(a);
+                float f2 = MathHelper.cos(a);
 
-            if (target.motionY < 0 && !target.isSneaking())
-            {
-                target.motionY = 0;
+                target.motionX = (double) (target.moveStrafing * f2 - target.moveForward * f1) * speed;
+                target.motionZ = (double) (target.moveForward * f2 + target.moveStrafing * f1) * speed;
+                target.motionY = -MathHelper.sin((float) (target.rotationPitch / 180 * Math.PI)) * target.moveForward * speed;
             }
+            else
+            {
+                target.motionX *= 0.6;
+                target.motionY *= 0.6;
+
+                if (target.motionY < 0)
+                {
+                    target.motionY = 0;
+                }
+            }
+        }
+        else
+        {
+            target.attackEntityFrom(DamageSource.drown, target.worldObj.getDifficulty().equals(EnumDifficulty.PEACEFUL) ? 1.0F : 0.5F);
         }
     }
 }
