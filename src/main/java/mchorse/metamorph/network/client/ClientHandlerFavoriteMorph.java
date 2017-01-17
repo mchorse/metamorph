480ce4b3a01877e52f911fee6dfd980beaebdee2
Jimmy Jenkins <mchorselessrider@gmail.com>
2017-01-17 12:11:43 +0000

    Fix server crash

--- src/main/java/mchorse/metamorph/network/client/ClientHandlerFavoriteMorph.java
+++ src/main/java/mchorse/metamorph/network/client/ClientHandlerFavoriteMorph.java
@@ -4,10 +4,13 @@ import mchorse.metamorph.ClientProxy;
 import mchorse.metamorph.capabilities.morphing.Morphing;
 import mchorse.metamorph.network.common.PacketFavoriteMorph;
 import net.minecraft.client.entity.EntityPlayerSP;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
 public class ClientHandlerFavoriteMorph extends ClientMessageHandler<PacketFavoriteMorph>
 {
     @Override
+    @SideOnly(Side.CLIENT)
     public void run(EntityPlayerSP player, PacketFavoriteMorph message)
     {
         Morphing.get(player).favorite(message.index);
--- src/main/java/mchorse/metamorph/network/client/ClientHandlerRemoveMorph.java
+++ src/main/java/mchorse/metamorph/network/client/ClientHandlerRemoveMorph.java
@@ -4,10 +4,13 @@ import mchorse.metamorph.ClientProxy;
 import mchorse.metamorph.capabilities.morphing.Morphing;
 import mchorse.metamorph.network.common.PacketRemoveMorph;
 import net.minecraft.client.entity.EntityPlayerSP;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
 public class ClientHandlerRemoveMorph extends ClientMessageHandler<PacketRemoveMorph>
 {
     @Override
+    @SideOnly(Side.CLIENT)
     public void run(EntityPlayerSP player, PacketRemoveMorph message)
     {
         Morphing.get(player).remove(message.index);
