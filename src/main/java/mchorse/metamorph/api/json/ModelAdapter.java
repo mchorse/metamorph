4c481cb9a8e16a4514a967c5bba543f45e3f8687
Jimmy Jenkins <mchorselessrider@gmail.com>
2017-01-18 18:44:42 +0000

    Make default property optional

--- src/main/java/mchorse/metamorph/api/json/ModelAdapter.java
+++ src/main/java/mchorse/metamorph/api/json/ModelAdapter.java
@@ -7,6 +7,7 @@ import com.google.gson.GsonBuilder;
 import com.google.gson.JsonDeserializationContext;
 import com.google.gson.JsonDeserializer;
 import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
 import com.google.gson.JsonParseException;
 
 import mchorse.metamorph.api.models.Model;
@@ -31,9 +32,14 @@ public class ModelAdapter implements JsonDeserializer<Model>
     public Model deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) throws JsonParseException
     {
         Model model = gson.fromJson(json, Model.class);
-        String type = json.getAsJsonObject().get("default").getAsString();
+        JsonObject object = json.getAsJsonObject();
 
-        model.defaultTexture = new ResourceLocation(type);
+        if (object.has("default"))
+        {
+            String type = object.get("default").getAsString();
+
+            model.defaultTexture = new ResourceLocation(type);
+        }
 
         return model;
     }
