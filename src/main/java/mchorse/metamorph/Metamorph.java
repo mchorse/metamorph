5c37b648c139de9d5949b3c2320a1be05aefa2e8
Jimmy Jenkins <mchorselessrider@gmail.com>
2017-01-27 20:58:19 +0000

    Bump version

--- src/main/java/mchorse/metamorph/Metamorph.java
+++ src/main/java/mchorse/metamorph/Metamorph.java
@@ -30,7 +30,7 @@ public class Metamorph
     /* Metadata fields */
     public static final String MODID = "metamorph";
     public static final String MODNAME = "Metamorph";
-    public static final String VERSION = "1.1-pre";
+    public static final String VERSION = "1.1";
 
     public static final String CLIENT_PROXY = "mchorse.metamorph.ClientProxy";
     public static final String SERVER_PROXY = "mchorse.metamorph.CommonProxy";
@@ -62,7 +62,7 @@ public class Metamorph
     /* Logging */
 
     // TODO: Set to false when publishing and remove all unnecessary printlns
-    public static boolean DEBUG = true;
+    public static boolean DEBUG = false;
     public static Logger LOGGER;
 
     /**
