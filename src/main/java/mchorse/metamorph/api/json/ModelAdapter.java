f0b147c80f767ce2ee1941cd05a91508fdf67428
Jimmy Jenkins <mchorselessrider@gmail.com>
2017-03-16 10:25:29 +0000

    Add JSON serializing adapters

--- src/main/java/mchorse/metamorph/api/json/ModelAdapter.java
+++ src/main/java/mchorse/metamorph/api/json/ModelAdapter.java
@@ -4,11 +4,15 @@ import java.lang.reflect.Type;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.JsonArray;
 import com.google.gson.JsonDeserializationContext;
 import com.google.gson.JsonDeserializer;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParseException;
+import com.google.gson.JsonPrimitive;
+import com.google.gson.JsonSerializationContext;
+import com.google.gson.JsonSerializer;
 
 import mchorse.metamorph.api.models.Model;
 import net.minecraft.util.ResourceLocation;
@@ -18,9 +22,9 @@ import net.minecraft.util.ResourceLocation;
  * 
  * This adapter is responsible for only deserializing a {@link Model} instance.
  */
-public class ModelAdapter implements JsonDeserializer<Model>
+public class ModelAdapter implements JsonDeserializer<Model>, JsonSerializer<Model>
 {
-    private Gson gson = new GsonBuilder().create();
+    public static Gson plainGSON = new GsonBuilder().create();
 
     /**
      * Deserializes {@link Model}
@@ -31,7 +35,7 @@ public class ModelAdapter implements JsonDeserializer<Model>
     @Override
     public Model deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) throws JsonParseException
     {
-        Model model = gson.fromJson(json, Model.class);
+        Model model = plainGSON.fromJson(json, Model.class);
         JsonObject object = json.getAsJsonObject();
 
         if (object.has("default"))
@@ -42,5 +46,50 @@ public class ModelAdapter implements JsonDeserializer<Model>
         }
 
         return model;
+    }
+
+    /**
+     * Serializes {@link Model}
+     * 
+     * This method is responsible for cleaning up some of Model's fields (to 
+     * make the file output more cleaner).
+     */
+    @Override
+    public JsonElement serialize(Model src, Type typeOfSrc, JsonSerializationContext context)
+    {
+        JsonElement serial = plainGSON.toJsonTree(src, typeOfSrc);
+        JsonObject map = serial.getAsJsonObject();
+
+        map.remove("model");
+        map.remove("defaultTexture");
+        map.remove("scale");
+        map.remove("limbs");
+        map.remove("poses");
+
+        if (!src.model.isEmpty())
+        {
+            map.addProperty("model", src.model);
+        }
+
+        if (src.defaultTexture != null)
+        {
+            map.addProperty("default", src.defaultTexture.toString());
+        }
+
+        if (src.scale[0] != 1 || src.scale[1] != 1 || src.scale[2] != 1)
+        {
+            JsonArray array = new JsonArray();
+
+            array.add(new JsonPrimitive(src.scale[0]));
+            array.add(new JsonPrimitive(src.scale[1]));
+            array.add(new JsonPrimitive(src.scale[2]));
+
+            map.add("scale", array);
+        }
+
+        map.add("limbs", context.serialize(src.limbs));
+        map.add("poses", context.serialize(src.poses));
+
+        return map;
     }
 }
