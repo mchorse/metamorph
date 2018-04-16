699fe088355c35fbe8c87fc1ac9d933d71fa81c8
Jimmy Jenkins <mchorselessrider@gmail.com>
2018-04-16 09:19:56 +0100

    Oh, that makes sense why I used reflection...

--- src/main/java/mchorse/metamorph/ClientProxy.java
+++ src/main/java/mchorse/metamorph/ClientProxy.java
@@ -1,5 +1,6 @@
 package mchorse.metamorph;
 
+import java.lang.reflect.Field;
 import java.util.Map;
 
 import mchorse.metamorph.api.MorphManager;
@@ -101,10 +102,41 @@ public class ClientProxy extends CommonProxy
     /**
      * Substitute default player renders to get the ability to render the
      * hand.
+     *
+     * Please, kids, don't do that at home. This was made by an expert in
+     * his field, so please, don't override skinMap the way I did. Don't break
+     * the compatibility with this mod (already confirmed breaking while 
+     * using Metamorph and Blockbuster together).
      */
+    @SuppressWarnings({"rawtypes", "unchecked"})
     private void substitutePlayerRenderers(RenderManager manager)
     {
-        Map<String, net.minecraft.client.renderer.entity.RenderPlayer> skins = manager.getSkinMap();
+        Map<String, net.minecraft.client.renderer.entity.RenderPlayer> skins = null;
+
+        /* Iterate over all render manager fields and get access to skinMap */
+        for (Field field : manager.getClass().getDeclaredFields())
+        {
+            if (field.getType().equals(Map.class))
+            {
+                field.setAccessible(true);
+
+                try
+                {
+                    Map map = (Map) field.get(manager);
+
+                    if (map.get("default") instanceof net.minecraft.client.renderer.entity.RenderPlayer)
+                    {
+                        skins = map;
+
+                        break;
+                    }
+                }
+                catch (Exception e)
+                {
+                    e.printStackTrace();
+                }
+            }
+        }
 
         /* Replace player renderers with Blockbuster substitutes */
         if (skins != null)
