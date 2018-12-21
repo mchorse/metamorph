e06d6ca026017d11d7a4da41b49e90c856d62705
McHorse <mchorselessrider@gmail.com>
2018-12-21 14:40:19 +0000

    Resolve two annoying bugs

--- src/main/java/mchorse/metamorph/Metamorph.java
+++ src/main/java/mchorse/metamorph/Metamorph.java
@@ -39,7 +39,7 @@ public class Metamorph
     /* Metadata fields */
     public static final String MODID = "metamorph";
     public static final String MODNAME = "Metamorph";
-    public static final String VERSION = "@VERSION@";
+    public static final String VERSION = "1.1.7";
 
     public static final String CLIENT_PROXY = "mchorse.metamorph.ClientProxy";
     public static final String SERVER_PROXY = "mchorse.metamorph.CommonProxy";
