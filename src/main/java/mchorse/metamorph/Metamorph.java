4b5995b6777fd0247d2b42f50f38c556f0f22948
McHorse <mchorselessrider@gmail.com>
2018-12-27 19:28:32 +0000

    Wrong dependency format

--- src/main/java/mchorse/metamorph/Metamorph.java
+++ src/main/java/mchorse/metamorph/Metamorph.java
@@ -33,7 +33,7 @@ import net.minecraftforge.fml.common.network.NetworkRegistry;
  * they're outdated), however, iChun saying that he's working on Morph for 
  * 1.10.2, this is really exciting! :D
  */
-@Mod(modid = Metamorph.MODID, name = Metamorph.MODNAME, version = Metamorph.VERSION, guiFactory = Metamorph.GUI_FACTORY, updateJSON = "https://raw.githubusercontent.com/mchorse/metamorph/master/version.json", dependencies = "after:moreplayermodels;mclib@[%MCLIB%,)")
+@Mod(modid = Metamorph.MODID, name = Metamorph.MODNAME, version = Metamorph.VERSION, guiFactory = Metamorph.GUI_FACTORY, updateJSON = "https://raw.githubusercontent.com/mchorse/metamorph/master/version.json", dependencies = "after:moreplayermodels;required-after:mclib@[%MCLIB%,)")
 public class Metamorph
 {
     /* Metadata fields */
