29d310fd4836ae98454071e552519c5472aac505
asanetargoss <asanetargoss@users.noreply.github.com>
2019-03-09 14:44:53 -0800

    Give accurate air count to entity tied to morph

--- src/main/java/mchorse/metamorph/api/morphs/EntityMorph.java
+++ src/main/java/mchorse/metamorph/api/morphs/EntityMorph.java
@@ -430,8 +430,7 @@ public class EntityMorph extends AbstractMorph
         entity.ticksExisted = target.ticksExisted;
         /* Fighting with death of entities like zombies */
         entity.setHealth(target.getHealth());
-        /* Prevent drowning sound of squids, guardians, etc... */
-        entity.setAir(300);
+        entity.setAir(cap.getHasSquidAir() ? cap.getSquidAir() : target.getAir());
 
         /* Now goes the code responsible for achieving somewhat riding 
          * support. This is ridiculous... */
