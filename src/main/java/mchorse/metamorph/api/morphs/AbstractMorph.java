40aec7449f913708b30103673bf3d47c462b561b
asanetargoss <asanetargoss@users.noreply.github.com>
2020-09-06 20:49:11 -0700

    Fix block morphs rendering hands

--- src/main/java/mchorse/metamorph/api/morphs/AbstractMorph.java
+++ src/main/java/mchorse/metamorph/api/morphs/AbstractMorph.java
@@ -165,7 +165,7 @@ public abstract class AbstractMorph
     @SideOnly(Side.CLIENT)
     public boolean renderHand(EntityPlayer player, EnumHand hand)
     {
-        return false;
+        return !getSettings().hands;
     }
 
     /* Update loop */
