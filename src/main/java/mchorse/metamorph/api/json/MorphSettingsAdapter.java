18acb533c64ff818ef86521fddee7bacb3c089aa
McHorse <mchorselessrider@gmail.com>
2020-11-29 10:37:40 +0000

    Make morph settings parsing more lenient

--- src/main/java/mchorse/metamorph/api/json/MorphSettingsAdapter.java
+++ src/main/java/mchorse/metamorph/api/json/MorphSettingsAdapter.java
@@ -29,32 +29,37 @@ public class MorphSettingsAdapter implements JsonDeserializer<MorphSettings>
         MorphSettings morph = new MorphSettings();
         MorphManager manager = MorphManager.INSTANCE;
 
-        if (object.has("health"))
+        if (object.has("health") && object.get("health").isJsonPrimitive())
         {
             morph.health = object.get("health").getAsInt();
         }
 
-        if (object.has("speed"))
+        if (object.has("speed") && object.get("speed").isJsonPrimitive())
         {
             morph.speed = object.get("speed").getAsFloat();
         }
 
-        if (object.has("hostile"))
+        if (object.has("hostile") && object.get("hostile").isJsonPrimitive())
         {
             morph.hostile = object.get("hostile").getAsBoolean();
         }
 
-        if (object.has("hands"))
+        if (object.has("hands") && object.get("hands").isJsonPrimitive())
         {
             morph.hands = object.get("hands").getAsBoolean();
         }
 
-        if (object.has("abilities"))
+        if (object.has("abilities") && object.get("abilities").isJsonArray())
         {
             morph.abilities.clear();
 
             for (JsonElement ability : object.get("abilities").getAsJsonArray())
             {
+                if (!ability.isJsonPrimitive())
+                {
+                    continue;
+                }
+
                 IAbility iability = manager.abilities.get(ability.getAsString());
 
                 if (iability != null)
@@ -64,17 +69,17 @@ public class MorphSettingsAdapter implements JsonDeserializer<MorphSettings>
             }
         }
 
-        if (object.has("action"))
+        if (object.has("action") && object.get("updates").isJsonPrimitive())
         {
             morph.action = manager.actions.get(object.get("action").getAsString());
         }
 
-        if (object.has("attack"))
+        if (object.has("attack") && object.get("updates").isJsonPrimitive())
         {
             morph.attack = manager.attacks.get(object.get("attack").getAsString());
         }
 
-        if (object.has("updates"))
+        if (object.has("updates") && object.get("updates").isJsonPrimitive())
         {
             morph.updates = object.get("updates").getAsBoolean();
         }
