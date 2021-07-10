34d319c3fadaa294c6c54bdb0febca36a4b151c0
NyaNLI <nyaplus@qq.com>
2021-07-10 20:27:10 +0800

    Add ISyncableMorph.resume()

--- src/main/java/mchorse/metamorph/api/morphs/utils/ISyncableMorph.java
+++ src/main/java/mchorse/metamorph/api/morphs/utils/ISyncableMorph.java
@@ -1,10 +1,39 @@
 package mchorse.metamorph.api.morphs.utils;
 
+import mchorse.metamorph.api.models.IMorphProvider;
 import mchorse.metamorph.api.morphs.AbstractMorph;
+import mchorse.metamorph.bodypart.BodyPart;
+import mchorse.metamorph.bodypart.IBodyPartProvider;
 
 public interface ISyncableMorph
 {
     public void pause(AbstractMorph previous, int offset);
 
     public boolean isPaused();
+
+    default public void resume()
+    {
+        Object morph = this;
+
+        if (morph instanceof IMorphProvider)
+        {
+            morph = ((IMorphProvider) morph).getMorph();
+        }
+        
+        if (morph instanceof IAnimationProvider)
+        {
+            ((IAnimationProvider) morph).getAnimation().paused = false;
+        }
+        
+        if (morph instanceof IBodyPartProvider)
+        {
+            for (BodyPart part : ((IBodyPartProvider) morph).getBodyPart().parts)
+            {
+                if (!part.morph.isEmpty() && part.morph.get() instanceof ISyncableMorph)
+                {
+                    ((ISyncableMorph) part.morph.get()).resume();
+                }
+            }
+        }
+    }
 }
